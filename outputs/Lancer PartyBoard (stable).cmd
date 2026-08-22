bfc765631b7fda4ff2dfd72121d3f43e12404e16
zeranemesis <valentin.samson27@gmail.com>
2026-08-22 21:37:28 +0200

    Add files via upload

---
+++ outputs/Lancer PartyBoard (stable).cmd
@@ -0,0 +1,4 @@
+@echo off
+pushd "C:\Users\valen\Documents\Codex\2026-08-22\j-aimerais-faire-une-d-compilation\work\partyboard-audio-local\build\audio-local-vs\RelWithDebInfo"
+start "" "partyboard.exe"
+popd
---
+++ outputs/Lancer PartyBoard - audio corrige.cmd
@@ -0,0 +1,4 @@
+@echo off
+pushd "C:\Users\valen\Documents\Codex\2026-08-22\j-aimerais-faire-une-d-compilation\work\partyboard-audio-local\build\aexp\RelWithDebInfo"
+start "" "partyboard.exe"
+popd
